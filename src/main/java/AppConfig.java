a212d3705fb8f552b7106b4f33f3e1ee1ab28504
Janamdeep <janam.1997.js@gmail.com>
2020-09-29 15:33:57 +0530

    autowired the setter injection

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,22 +2,27 @@ import com.practice.repository.HibernateSpeakerRepositoryImpl;
 import com.practice.repository.SpeakerRepository;
 import com.practice.service.SpeakerService;
 import com.practice.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.practice"})
 public class AppConfig {
 
-    @Bean(name = "speakerService")
+   /* @Bean(name = "speakerService")
+    @Scope(value= BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService(){
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
 //        service.setRepository(getSpeakerRepository());
-//
         return service;
-    }
+    }*/
 
-    @Bean(name = "speakerRepository")
+    /*@Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return new HibernateSpeakerRepositoryImpl();
-    }
+    }*/
 }
--- src/main/java/com/practice/service/SpeakerServiceImpl.java
+++ src/main/java/com/practice/service/SpeakerServiceImpl.java
@@ -3,14 +3,22 @@ package com.practice.service;
 import com.practice.model.Speaker;
 import com.practice.repository.HibernateSpeakerRepositoryImpl;
 import com.practice.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
     public SpeakerServiceImpl (SpeakerRepository speakerRepository){
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,7 +27,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
